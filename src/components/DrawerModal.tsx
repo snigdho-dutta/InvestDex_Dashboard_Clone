5bf33de58ca4dc491476f73cd8bdd759d2490170
Snigdho64 <aexseusidparker64@gmail.com>
2022-04-29 13:48:27 +0530

    fixed design loginmodal

--- src/components/DrawerModal.tsx
+++ src/components/DrawerModal.tsx
@@ -10,24 +10,29 @@ const DrawerModal = () => {
   const [darkMode, setDarkMode] = useRecoilState(darkState)
 
   return (
-    <div className="fixed p-3 flex items-center flex-col bottom-[2rem] left-[1rem] sm:left-[10rem] z-10 bg-white rounded-2xl shadow-lg drop-shadow min-h-[200px] min-w-[300px] justify-around">
+    <div className="fixed p-3 flex items-center flex-col bottom-[2rem] left-[1rem] sm:left-[10rem] z-10 bg-white dark:bg-slate-900 rounded-2xl shadow-lg drop-shadow min-h-[200px] min-w-[300px] justify-around">
       <div className="text-emerald-500 flex space-x-2 items-center border-b pb-2 border-emerald-500 text-semibold w-fit h-4 whitespace-nowrap">
         <IoLogOutOutline />
         <p>Login to InvestDex</p>
       </div>
-      <div className="flex flex-col text-gray-500 w-full">
-        <div className="flex justify-between">
-          <div>Help & Support</div>
-          <div>Notifications</div>
+      <div className="flex flex-col text-gray-500 dark:text-slate-200 w-full">
+        <div className="flex justify-between text-left">
+          <p className="w-1/2">Help & Support</p>
+          <p className="w-1/2">Notifications</p>
         </div>
         <div className="flex justify-between">
-          <div>Personal Info</div>
+          <p className="w-1/2">Personal Info</p>
           <button
-            className="flex items-center space-1"
+            className={`flex w-1/2 items-center space-x-2
+            `}
             onClick={() => setDarkMode((p) => !p)}
           >
             <p>{!darkMode ? 'Light Mode' : 'Dark Mode'}</p>{' '}
-            {!darkMode ? <BsToggleOff size={26} /> : <BsToggleOn size={26} />}
+            {!darkMode ? (
+              <BsToggleOff size={26} className="text-gray-700" />
+            ) : (
+              <BsToggleOn size={26} className="text-emerald-500" />
+            )}
           </button>
         </div>
         <div className="flex items-center gap-x-2">
@@ -36,7 +41,7 @@ const DrawerModal = () => {
           <IoIosArrowForward className="rotate-90 bg-gray-500/50 text-white rounded-full " />
         </div>
       </div>
-      <button className="p-1 rounded-md bg-customBlue text-white self-start">
+      <button className="p-1 px-2 rounded-md bg-customBlue text-white self-start">
         Upgrade
       </button>
     </div>
